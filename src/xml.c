82f56c8abecc1b2d8b76a30057b85ab2fb4586c4
s_baoshan <s_baoshan@hotmail.com>
2016-03-26 21:41:33 +0800

    xml.c, read timenode from xmldoc

--- src/xml.c
+++ src/xml.c
@@ -590,13 +590,20 @@ uint8 func_minfo(uint8 dev, uint8 xml_idx)
 		
 		return retErr;
 }
+
+int one_his_data(pHis_data pHis)
+{
+    return NO_ERR;
+}
+
 //抄表数据上传
 uint8 func_rptup(uint8 dev, uint8 xml_idx)
 {
 	uint8 err = NO_ERR;
 	FILE *fp;
+    char pErr[100];
 	xml_info_str l_xmlInfo;
-	
+	int total_row;
 	err = getXmlInfo(dev,&l_xmlInfo);
 	if(err != NO_ERR){
 		printf("getXmlInfo Error.\n");
@@ -606,19 +613,28 @@ uint8 func_rptup(uint8 dev, uint8 xml_idx)
 	printf("l_xmlInfo.func_type: %u, l_xmlInfo.oper_type: %u\n", l_xmlInfo.func_type, l_xmlInfo.oper_type);
 	switch(l_xmlInfo.oper_type){
 	case em_OPER_RD:
-		printf("now make his data xml.\n");
-		//一次性读取所有满足条件的历史放到临时链表里
-		//	解析到时间点信息, 传入read_his_data()
-		//每次只遍历5个
-		//每遍历一行数据, 就形成一帧xml
-		//待5个全部组帧完毕, 或到达链表底层, 加上帧尾
-		//发送到上位机
-		//等待上位机返回成功应答, 否则一直发送本包, 重复5次(约定)
-		//进行下一包的组合
-		
-		//获取共有多少行数据
-		//按照每包插入的条数(预先约定), 拆分成多少包
-		while(1/*数据没有发送完成*/){
+	    printf("now make his data xml.\n");
+        pXml_info pXmlinfo = malloc(sizeof(xml_info_str));
+        getXmlInfo(dev, pXmlinfo);
+        xmlDocPtr pDoc = pXmlinfo->xmldoc;
+        xmlNodePtr curNode = xmlDocGetRootElement(pDoc);
+        curNode = curNode->children;//<common>node
+        char timenode[30]={0};
+        while(curNode) {
+            if(xmlStrEqual(curNode->name, CONST_CAST "time")) {
+                strcpy(timenode, (char*)xmlNodeGetContent(curNode->xmlChildrenNode));
+            }
+            curNode = curNode->next;
+        }
+        
+        read_his_data(timenode,em_heat,pErr);//每个线程读取历史数据时, 都会创建独立的链表
+        
+        total_row = get_his_cnt(em_heat);
+        
+        retrieve_his_data(em_heat,5,one_his_data);
+        
+        
+        while(1/*数据没有发送完成*/){
 			
 			err = makexml(&l_xmlInfo,xml_idx);
 			if(err == NO_ERR){
@@ -627,7 +643,7 @@ uint8 func_rptup(uint8 dev, uint8 xml_idx)
 				fclose(fp);
 			}
 		}
-			
+        free(pXmlinfo);
 	case em_OPER_WR:
 		break;
 	case em_OPER_ASW:
@@ -640,7 +656,6 @@ uint8 func_rptup(uint8 dev, uint8 xml_idx)
 	}
 	return err;
 }
-
 
 uint8 func_rdstat(uint8 dev, uint8 xml_idx)
 {
