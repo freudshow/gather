a7ad462052c0a564cae4321f68c55e8caf2218f7
s_baoshan <s_baoshan@hotmail.com>
2016-04-07 11:46:41 +0800

    调整协议和sql语句

--- docs/design.files/prog.design/sqlfile.sql
+++ docs/design.files/prog.design/sqlfile.sql
@@ -10,20 +10,20 @@ create table t_base_define
 	f_config_value	varchar(50)
 );
 
-insert into t_base_define(f_config_name, f_config_value)values('primary_server', '122.5.18.174');
-insert into t_base_define(f_config_name, f_config_value)values('primary_dns', '219.146.0.130');
-insert into t_base_define(f_config_name, f_config_value)values('primary_port', '9026');
-insert into t_base_define(f_config_name, f_config_value)values('second_server', '219.146.0.130');
-insert into t_base_define(f_config_name, f_config_value)values('second_dns', '219.146.0.130');
-insert into t_base_define(f_config_name, f_config_value)values('second_port', '9027');
-insert into t_base_define(f_config_name, f_config_value)values('gateway_id', '5001');
-insert into t_base_define(f_config_name, f_config_value)values('net_type', '0/1/2');
-insert into t_base_define(f_config_name, f_config_value)values('md5_key', '863CA654');
-insert into t_base_define(f_config_name, f_config_value)values('aes_key', 'd41d8cd98f00b204e9800998ecf8427e');
-insert into t_base_define(f_config_name, f_config_value)values('collect_mode', '0/1');
-insert into t_base_define(f_config_name, f_config_value)values('collect_cycle', '15');
-insert into t_base_define(f_config_name, f_config_value)values('report_mode', '0/1');
-insert into t_base_define(f_config_name, f_config_value)values('beat_cycle', '2');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(1, 'primary_server', '122.5.18.174');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(2, 'primary_dns', '219.146.0.130');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(3, 'primary_port', '9026');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(4, 'second_server', '219.146.0.130');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(5, 'second_dns', '219.146.0.130');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(6, 'second_port', '9027');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(7, 'gateway_id', '5001');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(8, 'net_type', '0');-- 网络类型. 0 - GPRS网络; 1 - 485网络; 2 - LAN
+insert into t_base_define(f_id, f_config_name, f_config_value)values(9, 'md5_key', '863CA654');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(10, 'aes_key', 'd41d8cd98f00b204e9800998ecf8427e');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(11, 'collect_mode', '0');-- 0-根据collect_cycle周期抄表, 1-根据[t_time_node].[f_time_node]定点采集。
+insert into t_base_define(f_id, f_config_name, f_config_value)values(12, 'collect_cycle', '15');
+insert into t_base_define(f_id, f_config_name, f_config_value)values(13, 'report_mode', '0');-- 数据上报模式. 0 - 主动上报; 1 - 被动请求.
+insert into t_base_define(f_id, f_config_name, f_config_value)values(14, 'beat_cycle', '2');
 ---------------------------------
 --仪表地址信息配置表
 ---------------------------------
@@ -40,17 +40,17 @@ create table t_meter_info
 	f_install_pos	varchar(50)
 );
 
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (10, 1, '11110021147872', 1, 0, '6#管道井');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (10, 2, '11110052310292', 7, 1, '5#管道井');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (20, 3, '11110021147872', 1, 0, '3#地下室');--德鲁热表
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (20, 4, '11110053110202', 0, 3, '3#地下室');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (40, 5, '1', 7, 6, '2#电表间');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (40, 5, '12', 7, 6, '2#电表间');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (40, 5, '123', 7, 6, '2#电表间');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (40, 5, '1234', 7, 6, '2#电表间');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (40, 5, '12345', 7, 6, '2#电表间');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (40, 6, '123456', 7, 6, '1#电表间');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values (40, 6, '1234567', 7, 6, '1#电表间');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('10', 1, '11110021147872', 1, 0, '6#管道井');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('10', 2, '11110052310292', 7, 1, '5#管道井');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('20', 3, '11110021147872', 1, 0, '3#地下室');--德鲁热表
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('20', 4, '11110053110202', 0, 3, '3#地下室');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('40', 5, '1', 7, 6, '2#电表间');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('40', 5, '12', 7, 6, '2#电表间');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('40', 5, '123', 7, 6, '2#电表间');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('40', 5, '1234', 7, 6, '2#电表间');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('40', 5, '12345', 7, 6, '2#电表间');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('40', 6, '123456', 7, 6, '1#电表间');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos) values ('40', 6, '1234567', 7, 6, '1#电表间');
 ---------------------------------
 --电表历史数据表
 ---------------------------------
@@ -133,29 +133,30 @@ create table t_request_data
 	f_col_type	varchar(20)
 );
 
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '01', 'f_cur_cold_E', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '02', 'f_cur_heat_E', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '03', 'f_heat_power', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '04', 'f_flowrate', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '05', 'f_accum_flow', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '06', 'f_in_temp', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '07', 'f_out_temp', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '08', 'f_accum_work_time', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '09', 'f_cur_time', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('20', '10', 'f_state', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('10', '01', 'f_cur_accum_flow', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('10', '02', 'f_flowrate', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('10', '03', 'f_settle_date_accum_flow', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('10', '04', 'f_settle_date', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('10', '05', 'f_accum_work_time', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('40', '01', 'f_active_total_electricity', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('40', '02', 'f_cur_voltage', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('40', '03', 'f_cur_current', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('40', '04', 'f_cur_active_power', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('40', '05', 'f_cur_freq', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('40', '06', 'f_cur_reactive_power', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('40', '07', 'f_cur_ apparent_power', 'varchar(50)');
-insert into t_request_data(f_meter_type, f_item_index, f_col_name, f_col_type)values('40', '08', 'f_cur_power_factor', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(1, '20', '01', 'f_cur_cold_E', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(2, '20', '02', 'f_cur_heat_E', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(3, '20', '03', 'f_heat_power', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(4, '20', '04', 'f_flowrate', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(5, '20', '05', 'f_accum_flow', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(6, '20', '06', 'f_in_temp', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(7, '20', '07', 'f_out_temp', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(8, '20', '08', 'f_accum_work_time', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(9, '20', '09', 'f_cur_time', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(10, '20', '10', 'f_state', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(11, '10', '01', 'f_cur_accum_flow', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(12, '10', '02', 'f_flowrate', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(13, '10', '03', 'f_settle_date_accum_flow', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(14, '10', '04', 'f_settle_date', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(15, '10', '05', 'f_accum_work_time', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(16, '40', '01', 'f_active_total_electricity', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(17, '40', '02', 'f_cur_voltage', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(18, '40', '03', 'f_cur_current', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(19, '40', '04', 'f_cur_active_power', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(20, '40', '05', 'f_cur_freq', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(21, '40', '06', 'f_cur_reactive_power', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(22, '40', '07', 'f_cur_apparent_power', 'varchar(50)');
+insert into t_request_data(f_id, f_meter_type, f_item_index, f_col_name, f_col_type)values(23, '40', '08', 'f_cur_power_factor', 'varchar(50)');
+
 ---------------------------------
 --时间点配置表
 ---------------------------------
