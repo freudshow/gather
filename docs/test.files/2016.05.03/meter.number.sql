6a06074c6c6c39f75ec40109e06f0ee236352d76
s_baoshan <s_baoshan@hotmail.com>
2016-05-05 16:30:10 +0800

    增加两个没有修改电阻的裸板

--- docs/test.files/2016.05.03/meter.number.sql
+++ docs/test.files/2016.05.03/meter.number.sql
@@ -11,7 +11,6 @@ create table t_meter_info
 	f_install_pos	varchar(50)
 );
 
-
 insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 1, '11110021120001', 7, 0, '6#undercore');
 insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 2, '11110021120002', 7, 0, '6#undercore');
 insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 3, '11110021120003', 7, 0, '6#undercore');
@@ -28,42 +27,36 @@ insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_ch
 insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 14, '11110021120014', 7, 0, '6#undercore');
 insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 15, '11110021120015', 7, 0, '6#undercore');
 insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 16, '11110021120016', 7, 0, '6#undercore');
-
-
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 17, '11110031185392', 7, 0, '6#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 18, '11110031185384', 7, 0, '7#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 19, '11110031185397', 7, 0, '8#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 20, '11110031185395', 7, 0, '9#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 21, '11110031185373', 7, 0, '10#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 22, '11110031185387', 7, 0, '11#undercore');
-
-
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 23, '11110031185368', 7, 0, '11#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 24, '11110031185391', 7, 0, '11#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 25, '11110031185369', 7, 0, '11#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 26, '11110031185370', 7, 0, '11#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 27, '11110031185377', 7, 0, '11#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 28, '11110031185378', 7, 0, '11#undercore');
-
-
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 29, '11110031185393', 7, 0, '12#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 30, '11110031185396', 7, 0, '13#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 31, '11110031185376', 7, 0, '14#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 32, '11110031185389', 7, 0, '15#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 33, '11110031185388', 7, 0, '16#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 34, '11110031185394', 7, 0, '17#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 35, '11110031185382', 7, 0, '18#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 36, '11110031185379', 7, 0, '19#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 37, '11110031185371', 7, 0, '20#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 38, '11110031185372', 7, 0, '21#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 39, '11110031185374', 7, 0, '22#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 40, '11110031185381', 7, 0, '23#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 41, '11110031185385', 7, 0, '24#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 42, '11110031185398', 7, 0, '25#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 43, '11110031185386', 7, 0, '26#undercore');
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 44, '11110031185383', 7, 0, '27#undercore');
-
-
-insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 45, '11110031185375', 7, 0, '28#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 17, '11110021120017', 7, 0, '6#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 18, '11110021120018', 7, 0, '6#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 19, '11110031185392', 7, 0, '6#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 20, '11110031185384', 7, 0, '7#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 21, '11110031185397', 7, 0, '8#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 22, '11110031185395', 7, 0, '9#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 23, '11110031185373', 7, 0, '10#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 24, '11110031185387', 7, 0, '11#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 25, '11110031185368', 7, 0, '11#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 26, '11110031185391', 7, 0, '11#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 27, '11110031185369', 7, 0, '11#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 28, '11110031185370', 7, 0, '11#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 29, '11110031185377', 7, 0, '11#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 30, '11110031185378', 7, 0, '11#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 31, '11110031185393', 7, 0, '12#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 32, '11110031185396', 7, 0, '13#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 33, '11110031185376', 7, 0, '14#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 34, '11110031185389', 7, 0, '15#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 35, '11110031185388', 7, 0, '16#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 36, '11110031185394', 7, 0, '17#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 37, '11110031185382', 7, 0, '18#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 38, '11110031185379', 7, 0, '19#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 39, '11110031185371', 7, 0, '20#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 40, '11110031185372', 7, 0, '21#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 41, '11110031185374', 7, 0, '22#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 42, '11110031185381', 7, 0, '23#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 43, '11110031185385', 7, 0, '24#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 44, '11110031185398', 7, 0, '25#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 45, '11110031185386', 7, 0, '26#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 46, '11110031185383', 7, 0, '27#undercore');
+insert into t_meter_info (f_meter_type, f_device_id, f_meter_address, f_meter_channel, f_meter_proto_type, f_install_pos)  values  ('20', 47, '11110031185375', 7, 0, '28#undercore');
 
 select * from t_meter_info;
