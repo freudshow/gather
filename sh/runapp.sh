fff35d5bd02d758c466f03fc0f570b35be0f9e96
s_baoshan <s_baoshan@hotmail.com>
2016-05-09 09:21:26 +0800

    rewrite start app script, it can execute app correctly

--- sh/runapp.sh
+++ sh/runapp.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-echo "======start gather app======"
-cd /app
-./gather_V0
-while [ $? == 1 ]; do
-    echo "======gather app aborted, re-run again======"
-    sleep 3
-    cd /app
-	./gather_V0
+
+while [ 1 ]
+do
+	count=`ps -ef |grep ./gather_V0 |grep -v grep |wc -l`
+
+	if [ $count -lt 1 ]
+    then
+		cd /app
+		./gather_V0&
+		echo "********************************************"
+		echo "              program restart!"
+		echo "********************************************"
+		sleep 10
+	fi
+	sleep 60
 done
